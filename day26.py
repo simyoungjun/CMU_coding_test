e64e6bda14250774c74629c33993fa6b57801449
SIM <simyoungjun@gmail.com>
2023-01-04 12:40:31 +0900

    26

--- day26.py
+++ day26.py
@@ -1,12 +1,29 @@
 #%% 저주의 숫자3
 def solution(n):
-    answer = 0
-    for i in range(1, n + 1):
-        t = '3' in str(i)
-        if i // 3 == 0:
-            answer += 1
+    i = 1
+    answer = []
+    count = 0
+    while count < n:
+        if i % 3 == 0 or '3' in str(i):
+            pass
         else:
-            answer += 1
-    return answer
+            answer.append(i)
+            count += 1
+        i += 1
+    return answer[-1]
 
-solution(10)
+solution(15)
+
+#%% 두 개 뽑아서 더하기
+def solution(numbers):
+    answer = []
+    l = len(numbers)
+    for i in range(l):
+        for j in range(i + 1, l):
+            s = numbers[i] + numbers[j]
+            if s in answer:
+                continue
+            answer.append(s)
+
+    return sorted(answer)
+solution([2,1,3,4,1])
